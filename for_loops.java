894ff1abb8a195bd847d2a884da027a1a251a342
unknown <vishuSharma13579>
2020-07-24 21:36:55 +0530

    initial commit

---
+++ for_loops.java
@@ -0,0 +1,7 @@
+public class{
+        public static void main(String[] args){
+                for(int i=0;i<3;i++){
+                         System.out.println("yahooo again... hello vishal");
+                }
+        }
+}
